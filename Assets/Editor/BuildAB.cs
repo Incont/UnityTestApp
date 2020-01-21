2721367926a53ceca26edb76d56d3369475e67d8
Artem <incontumaciam@gmail.com>
2020-01-21 20:07:07 +0300

    initial commit

---
+++ Assets/Editor/BuildAB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+public static class AssetBundles
+{
+    [MenuItem("Build/Do")]
+    public static void BuildAssetBundles()
+    {
+        var path = "AssetBundles" + DateTime.Now.ToString("_dd-MM-yyyy-HH-mm-ss");
+        Directory.CreateDirectory(path);
+        var manifest =  BuildPipeline.BuildAssetBundles(path,
+        BuildAssetBundleOptions.AppendHashToAssetBundleName,
+        BuildTarget.iOS);
+    }
+
+    //[PostProcessScene()]
+    private static void EmptyMethod()
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
